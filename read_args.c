6733a0ab314f52b0fe3b90b0f3a34ae5d32709b4
ArthurWalraven <arthurwalravenaw@gmail.com>
2020-08-25 13:43:25 -0300

    Housekeeping

--- read_args.c
+++ read_args.c
@@ -5,15 +5,16 @@ Params process_arguments(const int argc, char * const argv[]) {
         .n = 5,
         .simulation_time = 5.0,
         .box_radius = 10.0 * 1e3,
-        .ups = 100.0,
+        .ups = 1000.0,
         .fps = 50.0,
         .resolution = 240,
+        .avg_speed = 20.0 * 1e3,
         .output_filename = "animation.gif"
     };
 
     struct option long_options[] = {
         {"n",           required_argument, NULL, 'n'},
-        {"t",           required_argument, NULL, 't'},
+        {"time",        required_argument, NULL, 't'},
         {"box-radius",  required_argument, NULL, 'b'},
         {"avg-speed",   required_argument, NULL, 's'},
         {"ups",         required_argument, NULL, 'u'},
@@ -28,8 +29,7 @@ Params process_arguments(const int argc, char * const argv[]) {
     while (-1 != (ch = getopt_long(argc, argv, "t:n:", long_options, &opt_index))) {
         int items_read;
 
-        switch (ch)
-        {
+        switch (ch) {
             case 'n':
                 items_read = sscanf(optarg, " %d", &parameters.n);
                 break;
@@ -72,7 +72,7 @@ Params process_arguments(const int argc, char * const argv[]) {
         }
 
         if (items_read != 1) {
-            fprintf(stderr, "Unknown argument '%s' for option '%s'.\n", optarg, long_options[opt_index].name);
+            fprintf(stderr, "Unrecognized argument '%s' for option '%s'.\n", optarg, long_options[opt_index].name);
             exit(EXIT_FAILURE);
         }
     }
