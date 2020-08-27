f82355992dcd78ac6afec6c1d6b27d1089f1c6c7
ArthurWalraven <arthurwalravenaw@gmail.com>
2020-08-27 12:44:54 -0300

    Major renaming

--- physics.c
+++ physics.c
@@ -6,41 +6,41 @@ inline float gravity(const vec a, const vec b) {
     return G / dist_sq(b, a);
 }
 
-inline float physics__max_radius(const ball b[], const int n) {
+inline float physics__max_radius(const atom a[], const int n) {
     float max_r = 0;
 
     for (int i = 0; i < n; ++i) {
-        max_r = fmaxf(max_r, b[i].r);
+        max_r = fmaxf(max_r, a[i].r);
     }
     
     return max_r;
 }
 
-static inline void sort_by_Y(ball b[], const int n) {
+static inline void sort_by_Y(atom a[], const int n) {
     for (int i = 1; i < n; ++i) {
-        if_unlikely (b[i].p.y > b[i-1].p.y) {
+        if_unlikely (a[i].p.y > a[i-1].p.y) {
             int j = i-2;
-            while ((j >= 0) && (b[j].p.y <= b[i].p.y)) {
+            while ((j >= 0) && (a[j].p.y <= a[i].p.y)) {
                 --j;
             }
 
-            const ball temp = b[i];
+            const atom temp = a[i];
             
-            memmove(&b[j+2], &b[j+1], (i - (j+1)) * sizeof(*b));
-            b[j+1] = temp;
+            memmove(&a[j+2], &a[j+1], (i - (j+1)) * sizeof(*a));
+            a[j+1] = temp;
         }
     }
 
     TEST(
         for (int i = 1; i < n; ++i) {
-            assert(b[i-1].p.y >= b[i].p.y);
-            assert(memcmp(&b[i-1], &b[i], sizeof(b[i])) && "This is very likely an error");
+            assert(a[i-1].p.y >= a[i].p.y);
+            assert(memcmp(&a[i-1], &a[i], sizeof(a[i])) && "This is very likely an error");
         }
     )
 }
 
 __always_inline
-static void adjust_for_collision(ball * a, ball * b) {
+static void adjust_for_collision(atom * a, atom * b) {
     const vec p = sub(b->p, a->p);
 
     const vec v = sub(a->v, b->v);
@@ -73,64 +73,64 @@ static void adjust_for_collision(ball * a, ball * b) {
     b->p.y += b->v.y * time_since_collision;
 }
 
-inline void physics__random_populate(ball b[], const int n, const float box_radius, const float avg_speed) {
+inline void physics__random_populate(atom a[], const int n, const float box_radius, const float avg_speed) {
     assert(n >= 0);
     assert(box_radius > 0);
 
     const float speed_range = 2 * avg_speed / sqrtf(2);
 
     for (int i = 0; i < n; ++i) {
-        b[i].p.x = (2 * randf() - 1) * box_radius;
-        b[i].p.y = (2 * randf() - 1) * box_radius;
-        b[i].v.x = (2 * randf() - 1) * speed_range;
-        b[i].v.y = (2 * randf() - 1) * speed_range;
-        b[i].r = 1 * 1e3;
+        a[i].p.x = (2 * randf() - 1) * box_radius;
+        a[i].p.y = (2 * randf() - 1) * box_radius;
+        a[i].v.x = (2 * randf() - 1) * speed_range;
+        a[i].v.y = (2 * randf() - 1) * speed_range;
+        a[i].r = 1 * 1e3;
     }
     // TODO: Balls could be inside one another
 
-    sort_by_Y(b, n);
+    sort_by_Y(a, n);
 
     TEST(
         for (int i = 0; i < n; ++i) {
-            assert(b[i].r > EPS);
+            assert(a[i].r > EPS);
         }
     )
 }
 
-inline void physics__update(ball b[], const int n, const float box_radius, const float ellapsed_time) {
+inline void physics__update(atom a[], const int n, const float box_radius, const float ellapsed_time) {
     //* Update positions
     for (int i = 0; i < n; ++i) {
-        b[i].p.x += b[i].v.x * ellapsed_time;
-        b[i].p.y += b[i].v.y * ellapsed_time;
+        a[i].p.x += a[i].v.x * ellapsed_time;
+        a[i].p.y += a[i].v.y * ellapsed_time;
     }
     //*/
 
     //* Ball-ball collision
     for (int i = 0; i < n-1; ++i) {
         for (int j = i+1; j < n; ++j) {
-            if_unlikely(dist_sq(b[i].p, b[j].p) < sq(b[i].r + b[j].r)) {
-                const float scale_factor = 1.0 / ((double) (b[i].r + b[j].r) - (double) norm(sub(b[j].p, b[i].p)));
+            if_unlikely(dist_sq(a[i].p, a[j].p) < sq(a[i].r + a[j].r)) {
+                const float scale_factor = 1.0 / ((double) (a[i].r + a[j].r) - (double) norm(sub(a[j].p, a[i].p)));
                 
-                ball A = {
-                    .p = mul(b[i].p, scale_factor),
-                    .v = mul(b[i].v, scale_factor),
-                    .r = b[i].r * scale_factor
+                atom A = {
+                    .p = mul(a[i].p, scale_factor),
+                    .v = mul(a[i].v, scale_factor),
+                    .r = a[i].r * scale_factor
                 };
-                ball B = {
-                    .p = mul(b[j].p, scale_factor),
-                    .v = mul(b[j].v, scale_factor),
-                    .r = b[j].r * scale_factor
+                atom B = {
+                    .p = mul(a[j].p, scale_factor),
+                    .v = mul(a[j].v, scale_factor),
+                    .r = a[j].r * scale_factor
                 };
 
                 adjust_for_collision(&A, &B);
 
                 const float reciprocal_scale_factor = 1.0 / scale_factor;
 
-                b[i].p = mul(A.p, reciprocal_scale_factor);
-                b[i].v = mul(A.v, reciprocal_scale_factor);
+                a[i].p = mul(A.p, reciprocal_scale_factor);
+                a[i].v = mul(A.v, reciprocal_scale_factor);
 
-                b[j].p = mul(B.p, reciprocal_scale_factor);
-                b[j].v = mul(B.v, reciprocal_scale_factor);
+                a[j].p = mul(B.p, reciprocal_scale_factor);
+                a[j].v = mul(B.v, reciprocal_scale_factor);
             }
         }
     }
@@ -138,30 +138,30 @@ inline void physics__update(ball b[], const int n, const float box_radius, const
 
     //* Ball-box collision
     for (int i = 0; i < n; ++i) {
-        if_unlikely(norm_max(b[i].p) > (box_radius - b[i].r)) {
+        if_unlikely(norm_max(a[i].p) > (box_radius - a[i].r)) {
             // Horizontal collisions
-            if_unlikely(b[i].p.x > (box_radius - b[i].r)) {
-                b[i].v.x *= -1;
-                b[i].p.x = -b[i].p.x + 2 * (box_radius - b[i].r);
+            if_unlikely(a[i].p.x > (box_radius - a[i].r)) {
+                a[i].v.x *= -1;
+                a[i].p.x = -a[i].p.x + 2 * (box_radius - a[i].r);
             }
-            else if_unlikely(b[i].p.x < -(box_radius - b[i].r)) {
-                b[i].v.x *= -1;
-                b[i].p.x = -b[i].p.x + 2 * -(box_radius - b[i].r);
+            else if_unlikely(a[i].p.x < -(box_radius - a[i].r)) {
+                a[i].v.x *= -1;
+                a[i].p.x = -a[i].p.x + 2 * -(box_radius - a[i].r);
             }
 
             // Vertical collisions
-            if_unlikely(b[i].p.y > (box_radius - b[i].r)) {
-                b[i].v.y *= -1;
-                b[i].p.y = -b[i].p.y + 2 * (box_radius - b[i].r);
+            if_unlikely(a[i].p.y > (box_radius - a[i].r)) {
+                a[i].v.y *= -1;
+                a[i].p.y = -a[i].p.y + 2 * (box_radius - a[i].r);
             }
-            else if_unlikely(b[i].p.y < -(box_radius - b[i].r)) {
-                b[i].v.y *= -1;
-                b[i].p.y = -b[i].p.y + 2 * -(box_radius - b[i].r);
+            else if_unlikely(a[i].p.y < -(box_radius - a[i].r)) {
+                a[i].v.y *= -1;
+                a[i].p.y = -a[i].p.y + 2 * -(box_radius - a[i].r);
             }
         }
     }
     //*/
 
     // Keep balls sorted
-    sort_by_Y(b, n);
+    sort_by_Y(a, n);
 }
--- physics.h
+++ physics.h
@@ -13,9 +13,9 @@ typedef struct {
     vec p;
     vec v;
     float r;
-} ball;
+} atom;
 
 
-void physics__random_populate(ball b[], const int n, const float box_radius, const float avg_speed);
-float physics__max_radius(const ball b[], const int n);
-void physics__update(ball b[], const int n, const float box_radius, const float ellapsed_time);
+void physics__random_populate(atom a[], const int n, const float box_radius, const float avg_speed);
+float physics__max_radius(const atom a[], const int n);
+void physics__update(atom a[], const int n, const float box_radius, const float ellapsed_time);
