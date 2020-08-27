107abeb74f6ce8b8a76ba9cbbb6903e1938aa91f
ArthurWalraven <arthurwalravenaw@gmail.com>
2020-08-27 18:10:50 -0300

    First Velocity Verlet

--- physics.c
+++ physics.c
@@ -22,8 +22,8 @@ inline void physics__lattice_populate(atom a[], const int n, const float box_rad
     }
 
     for (int i = 0; i < n; ++i) {
-        a[i].v.x = (2 * randf() - 1) * box_radius;
-        a[i].v.y = (2 * randf() - 1) * box_radius;
+        a[i].v.x = (2 * randf() - 1) * 1;
+        a[i].v.y = (2 * randf() - 1) * 1;
         // a[i].v.x = 0;
         // a[i].v.y = 0;
 
@@ -56,39 +56,67 @@ static inline void sort_by_Y(atom a[], const int n) {
 }
 
 inline void physics__update(atom a[], const int n, const float ellapsed_time, const float box_radius) {
-    //*
     for (int i = 0; i < n; ++i) {
+        a[i].v.x += 0.5 * a[i].a.x * ellapsed_time;
+        a[i].v.y += 0.5 * a[i].a.y * ellapsed_time;
         a[i].r.x += a[i].v.x * ellapsed_time;
         a[i].r.y += a[i].v.y * ellapsed_time;
+        a[i].a.x = 0;
+        a[i].a.y = 0;
     }
-    //*/
 
-    //* Atom-box collision
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = i+1; j < n; ++j) {
+            vec dr = sub(a[j].r, a[i].r);
+
+            if_unlikely(norm_max(dr) > box_radius) {
+                if(dr.x > box_radius) {
+                    dr.x -= 2 * box_radius;
+                }
+                else if(dr.x < -box_radius) {
+                    dr.x += 2 * box_radius;
+                }
+
+                if(dr.y > box_radius) {
+                    dr.y -= 2 * box_radius;
+                }
+                else if(dr.y < -box_radius) {
+                    dr.y += 2 * box_radius;
+                }
+            }
+
+
+            const float recip_rr = 1/dot(dr, dr);
+            const vec acc = mul(dr, -24 * recip_rr * ( 2 * powf(recip_rr, 6) - powf(recip_rr, 3)));
+
+            a[i].a = add(a[i].a, acc);
+            a[j].a = sub(a[j].a, acc);
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        a[i].v.x += 0.5 * a[i].a.x * ellapsed_time;
+        a[i].v.y += 0.5 * a[i].a.y * ellapsed_time;
+    }
+
     for (int i = 0; i < n; ++i) {
         if_unlikely(norm_max(a[i].r) > box_radius) {
-            // Horizontal collisions
             if_unlikely(a[i].r.x > box_radius) {
-                a[i].v.x *= -1;
-                a[i].r.x = -a[i].r.x + 2 * box_radius;
+                a[i].r.x -= 2 * box_radius;
             }
             else if_unlikely(a[i].r.x < -box_radius) {
-                a[i].v.x *= -1;
-                a[i].r.x = -a[i].r.x - 2 * box_radius;
+                a[i].r.x += 2 * box_radius;
             }
 
-            // Vertical collisions
             if_unlikely(a[i].r.y > box_radius) {
-                a[i].v.y *= -1;
-                a[i].r.y = -a[i].r.y + 2 * box_radius;
+                a[i].r.y -= 2 * box_radius;
             }
             else if_unlikely(a[i].r.y < -box_radius) {
-                a[i].v.y *= -1;
-                a[i].r.y = -a[i].r.y - 2 * box_radius;
+                a[i].r.y += 2 * box_radius;
             }
         }
     }
-    //*/
-    
+
     // Keep atoms sorted
     sort_by_Y(a, n);
 }
